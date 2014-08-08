0dc2230bd0c5131a5401c453f8db004a7cd8ba1d
mar10j <youthefluff@gmail.com>
2014-08-08 14:07:51 -0400

    .strip(), .title(), if statements

---
+++ School.py
@@ -0,0 +1,34 @@
+name = input ("What is your name?")
+name = name.title().strip()
+if name == ("Jack") or name == ("William") or name == ("Bill nye"):
+    print("Correct.")
+else print("Nope")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
