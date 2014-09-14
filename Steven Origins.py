ee1897c708996b0850f68697909f3008a790eac6
mar10j <youthefluff@gmail.com>
2014-09-13 23:31:17 -0400

    STUFF

---
+++ Steven Origins.py
@@ -0,0 +1,6 @@
+print=input("WELCOME TO THE ADVENTURES OF STEVEN!")
+name = input("What is your name? >").strip().title()
+if name == ("Steven"):
+    print("Correct!")
+else:
+    print("No, your name is Steven, Steven.")
--- Teaching code.py
+++
@@ -1,4 +0,0 @@
-name = input("Wassup you poophead, What is your name? > ").strip().title()
-print("Nice to meet you, " + name)
-hobby = input("What is your hobby? > ").strip().title()
-print("OMG I TOTES LOVE " + hobby)
