dbdede808f3f354a1f756266a14740e9eef8ba63
mar10j <youthefluff@gmail.com>
2014-09-18 15:07:10 -0400

    silly little game I was making to teach soren

--- Steven Origins.py
+++ Steven Origins.py
@@ -1,6 +1,16 @@
-print=input("WELCOME TO THE ADVENTURES OF STEVEN!")
+input("WELCOME TO THE ADVENTURES OF STEVEN!     JACK MARTIN EDITION")
 name = input("What is your name? >").strip().title()
-if name == ("Steven"):
+
+if name == "Steven":
     print("Correct!")
-else:
-    print("No, your name is Steven, Steven.")
+
+if name != "Steven":
+    print("No your name is steven.")
+
+input("Now steven, this is your story. Okay? Good.")
+input("One day you were walking around when you see a short Curly haired boy walking down the street. Being the evil creature that you are, You decide to jump inside this poor boys hair.")
+di = input("As you examine the area, You realize that they hair is like a maze. Steven decides to go left, NOT RIGHT. >").strip().lower()
+if di == ("left"):
+    jackson = input("Good! Glad to see you are a rule follower, Steven. Now you see a evil Micheal Jackson! Will you attack or Flee?").strip().lower()
+    if jackson == ("attack"):
+        print("Violence is not the answer Steven! No!")
