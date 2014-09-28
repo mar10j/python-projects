b6812f843224c9fa43560a2bb43599045080cf9c
mar10j <youthefluff@gmail.com>
2014-09-28 09:55:02 -0400

    WHILE TRUE YAY

---
+++ Understanding While true.py
@@ -0,0 +1,12 @@
+def gh():
+    derp = input("What is your name?")
+    print("Hello there, " + derp)
+    while True: gag()
+def gag():
+    color = input("What is your favorite color")
+    print(color)
+    while True: gh()
+
+#It works the same with loading funct. Just better practice for later codes
+
+while True: gag()
