3fee00f927c02085e4c219a72f27c5b036ee0282
mar10j <youthefluff@gmail.com>
2014-10-04 18:18:58 -0400

    ooooooooh

--- soren loop.py
+++ soren loop.py
@@ -1,52 +1,64 @@
 from turtle import Turtle
 from random import random
 
-
 stevenjr = Turtle()
-stevenjr.pensize(10)
+stevenjr.pensize(1)
 def random_color():
     return(random(),random(),random())    
-       
 
 while True:
     for count in range(4):
         stevenjr.pencolor(random_color())
         stevenjr.forward(100)
         stevenjr.right(90)    
-    stevenjr.right(10)
+    stevenjr.right(1)
     for count in range(4):
         stevenjr.pencolor(random_color())
         stevenjr.forward(110)
         stevenjr.right(90)    
-    stevenjr.right(10)
+    stevenjr.right(1)
     for count in range(4):
         stevenjr.pencolor(random_color())
         stevenjr.forward(120)
         stevenjr.right(90)    
-    stevenjr.right(10)
+    stevenjr.right(1)
     for count in range(4):
         stevenjr.pencolor(random_color())
         stevenjr.forward(130)
         stevenjr.right(90)    
-    stevenjr.right(10)
+    stevenjr.right(1)
     for count in range(4):
         stevenjr.pencolor(random_color())
         stevenjr.forward(140)
         stevenjr.right(90)    
-    stevenjr.right(10)
+    stevenjr.right(1)
     for count in range(4):
         stevenjr.pencolor(random_color())
         stevenjr.forward(150)
         stevenjr.right(90)    
-    stevenjr.right(10)
+    stevenjr.right(1)
     for count in range(4):
         stevenjr.pencolor(random_color())
         stevenjr.forward(160)
         stevenjr.right(90)    
-    stevenjr.right(10)
+    stevenjr.right(1)
     for count in range(4):
         stevenjr.pencolor(random_color())
         stevenjr.forward(170)
-        stevenjr.right(90)    
-    stevenjr.right(10)
-    
+        stevenjr.right(90)
+    stevenjr.right(1)
+    for count in range(4):
+        stevenjr.pencolor(random_color())
+        stevenjr.forward(180)
+        stevenjr.right(90)
+    stevenjr.right(1)
+    for count in range(4):
+        stevenjr.pencolor(random_color())
+        stevenjr.forward(190)
+        stevenjr.right(90)
+    stevenjr.right(1)
+    for count in range(4):
+        stevenjr.pencolor(random_color())
+        stevenjr.forward(200)
+        stevenjr.right(90)
+    stevenjr.right(1)
