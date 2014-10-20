71c1d1d473029372237c6637f0c3aa394c1a8622
mar10j <youthefluff@gmail.com>
2014-10-19 20:19:22 -0400

    LOL INTERNET
    Show this to mr rob future self

---
+++ Testing classes.py
@@ -0,0 +1,10 @@
+class Direction():
+    def direct():
+        roaddi = input("Do you want to head Up, Down, Left or Right?").strip().lower()
+
+class Road_one(Direction):
+    if roaddi == input ("up"):
+        print("cool")
+
+direct()
+
